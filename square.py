2393836d58056b79f69b939b675c1ad78780b148
“Bhumika <bhumikag.btech23@rvu.edu.in>
2025-04-04 12:27:52 +0530

    Addind sqaure.py file

---
+++ square.py
@@ -0,0 +1,9 @@
+"""This module provides a function to square a number."""
+
+def square(num):
+    """Returns the square of the given number."""
+    return num ** 2
+
+if __name__ == "__main__":
+    print(square(1))  # Example usage
+
